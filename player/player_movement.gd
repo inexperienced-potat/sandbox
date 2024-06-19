9be180f2d43c4be4b91827fcc504881184696e09
elpharae <boura.frantisek01@gmail.com>
2024-06-19 15:55:46 +0200

    added camera, polished code

--- player/player_movement.gd
+++ player/player_movement.gd
@@ -1,14 +1,9 @@
 extends CharacterBody2D
 
 @export var movement_speed = 100
-var movement_velocity = Vector2(0, 0)
-
-func _ready():
-	pass
 
 func _physics_process(delta):
-	move_and_collide(movement_velocity)
+	move_and_collide(velocity)
 
 func _process(delta):
-	movement_velocity = Input.get_vector("left", "right", "up", "down") * movement_speed * delta 
-	velocity = movement_velocity
+	velocity = Input.get_vector("left", "right", "up", "down") * movement_speed * delta 
